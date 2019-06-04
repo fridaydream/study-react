acc5fc628cd92932f4ca8892547833986a3a465f
609457773@qq.com <609457773@qq.com>
2019-06-04 09:42:43 +0800

    feature: react chat01

---
+++ my-app/src/c01/ChatApp.js
@@ -0,0 +1,46 @@
+import React from "react";
+import withTimer from "../c06/withTimer";
+
+class MessageList extends React.PureComponent {
+  render() {
+    return <ul>{ this.props.messages.map(msg => <li>{msg}</li>) }</ul>;
+  }
+}
+
+export class ChatApp extends React.Component {
+  state = {
+    messages: [],
+    inputMsg: "",
+  };
+
+  handleInput = evt => {
+    this.setState({
+      inputMsg: evt.target.value
+    })
+  }
+
+  handleSend = () => {
+    const text = this.state.inputMsg
+    if (text) {
+      const newMessages = [...this.state.messages, text]
+      this.setState({
+        messages: newMessages
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <MessageList messages={this.state.messages} />
+        <div>
+          <input value={this.state.inputMsg} onChange={this.handleInput} />
+          <button onClick={this.handleSend}>Send</button>
+        </div>
+        <h2>{this.props.time.toLocaleString()}</h2>
+      </div>
+    )
+  }
+}
+
+export default withTimer(ChatApp);
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import ChatApp from "./c01/ChatApp";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import "antd/dist/antd.css";
+import "./index.css";
+
+const styles = {
+  fontFamily: "sans-serif",
+  paddingLeft: "250px",
+};
+
+const routeMap = {
+  chat: ChatApp,
+};
+
+class App extends React.PureComponent {
+  handleLinkClick = key => {
+    // window.location.hash = `#${key}`;
+    window.history.pushState(null, "", `/#/${key}`);
+    this.forceUpdate();
+  };
+  render() {
+    const currentPage = document.location.hash.replace(/#\/?/, "");
+    console.log('currentPage', currentPage)
+    let CurrentPage = routeMap[currentPage] || Hello;
+    return (
+      <div style={styles}>
+        <ul className="menu-list">
+          {Object.keys(routeMap).map(key => (
+            <li
+              key={key}
+              className={key === currentPage ? "is-active" : ""}
+              style={{ listStyle: "none" }}
+            >
+              <span className="link" onClick={() => this.handleLinkClick(key)}>
+                {key}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div style={{ padding: "30px 0" }}>
+          <CurrentPage />
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
